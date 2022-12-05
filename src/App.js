d34111d2470f38220b82c5651b99e76cea9be449
Michael Robinson <michael.jr.robinson@gmail.com>
2022-12-04 23:14:42 -0500

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+/* #1 */
+
+/* Setting useState */
+const [dotClick, setDotClick] = useState(0);
+
+/* plus counting function to run inside button  */
+function plusClick() {
+  setDotClick(dotClick + 1);
+}
+
+/* minus function to run inside button  */
+function minusClick() {
+  setDotClick(dotClick - 1);
+}
+
+
+
+
+
+
+/* #2 */
+
+/* Setting useState */
+const [dotClick2, setDotClick2] = useState(0);
+
+/* counting function to run inside button  */
+function plusClick2() {
+  setDotClick2(dotClick2 + 1);
+}
+
+/* minus function to run inside button  */
+function minusClick2() {
+  setDotClick2(dotClick2 - 1);
+}
+
+
+
+/* reset function to run inside button  */
+function buttonReset() {
+  setDotClick(0);
+  setDotClick2(0);
+}
+
+
+
+
+
+/*
+setInterval(() => {
+
+  setDotClick(dotClick + 1);
+  
+}, 1000);
+*/
+
+/* calling all the functions  */
+
+return (
+
+<div id="counter-container">
+
+<h1 id="score-keeper">Score Keeper</h1>
+
+<div id="players">
+<p id="player-1">Player 1</p>
+<p id="player-2">Player 2</p>
+</div>
+
+<div id="counter-display">
+<p id="counter-display1">{dotClick}</p>
+<p id="counter-display2">{dotClick2}</p>
+</div>
+
+
+<div id="player-buttons">
+<div id='player-1-buttons'>
+<button onClick={plusClick}>+</button>
+<button onClick={minusClick}>-</button>
+</div>
+
+<div id='player-2-buttons'>
+<button onClick={plusClick2}>+</button>
+<button onClick={minusClick2}>-</button>
+</div>
+</div>
+
+<button id="reset" onClick={buttonReset}></button>
+
+
+
+</div> /* main container  */
   );
 }
 
