265e3d6f77bcfe0b292e94d6891b0db5fcd99102
Michael Robinson <michael.jr.robinson@gmail.com>
2023-06-03 18:32:08 -0400

    Responsive updates.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { useState } from 'react';
-
+import mark from './underscore.png';
 
 
 
@@ -39,6 +39,7 @@ function plusClick2() {
 }
 
 /* minus function to run inside button  */
+
 function minusClick2() {
   setDotClick2(dotClick2 - 1);
 }
@@ -52,9 +53,6 @@ function buttonReset() {
 }
 
 
-
-
-
 /*
 setInterval(() => {
 
@@ -63,18 +61,40 @@ setInterval(() => {
 }, 1000);
 */
 
+
+
+
+
+
+
+
+
+
+
 /* calling all the functions  */
 
 return (
 
-<div id="counter-container">
+<body>
 
-<h1 id="score-keeper">Score Keeper</h1>
+
+<div id="inner-container">
+
+<h1 id="score-keeper-logo"></h1>
+
 
 <div id="players">
-<p id="player-1">Player 1</p>
-<p id="player-2">Player 2</p>
+<p id="player-1">PLAYER 1</p>
+<p id="player-2">PLAYER 2</p>
 </div>
+
+
+<div id="marks">
+<p id="mark-1"><img src={mark}/></p>
+<p id="mark-2"><img src={mark}/></p>
+</div>
+
+
 
 <div id="counter-display">
 <p id="counter-display1">{dotClick}</p>
@@ -84,21 +104,24 @@ return (
 
 <div id="player-buttons">
 <div id='player-1-buttons'>
-<button onClick={plusClick}>+</button>
-<button onClick={minusClick}>-</button>
+<button onClick={plusClick} id="plus"><p></p></button>
+<button onClick={minusClick} id="minus"><p></p></button>
 </div>
 
 <div id='player-2-buttons'>
-<button onClick={plusClick2}>+</button>
-<button onClick={minusClick2}>-</button>
+<button onClick={plusClick2} id="plus"><p></p></button>
+<button onClick={minusClick2} id="minus"><p></p></button>
 </div>
 </div>
+
 
 <button id="reset" onClick={buttonReset}></button>
 
 
+<div id="copyright">Copyright® Michael Robinson</div>
+</div> {/* inner container  */}
 
-</div> /* main container  */
+</body>  
   );
 }
 
---
+++ src/ReadMe by Michael.md
@@ -0,0 +1,21 @@
+Purpose
+I built this app to challenge myself to learn react usestate. 
+
+Result
+Created a scoreboard app with plus, minus, reset buttons for each player 
+score. 
+
+Code
+Used UseState to store functions/data. Two increment functions 
+on click and one reset function that resets back to 0 on a click. 
+
+Design
+I drew custom + and - chalk buttons and the chalk logo, 
+created in Adobe Illustrator.
+
+Issues
+Initially I didn't have a reset but figured out that just changing the 
+value to "0" resets the score board scores.
+
+Author
+Michael Robinson, August 2022
